f50080546c015fc40b54beb8a843cd0bcda405a1
Callum Sontag <callum.sontag@mettle.co.uk>
2023-09-18 10:08:52 +0100

    fixed delete asset modal

--- static/assets.js
+++ static/assets.js
@@ -1,6 +1,9 @@
 // https://stackoverflow.com/a/15133763
+
 document.onreadystatechange = function () {
   if (document.readyState == "complete") {
+    const logout = document.querySelector("#logoutButton");
+    logout.onclick = onLogoutEventHandler;
     const deleteForms = document.querySelectorAll(".deleteForm");
     for (let i = 0; i < deleteForms.length; i++) {
       deleteForms[i].onsubmit = onDeleteEventHandler;
@@ -8,15 +11,7 @@ document.onreadystatechange = function () {
   }
 };
 
-document.onreadystatechange = function () {
-  if (document.readyState == "complete") {
-    const logout = document.querySelector("#logoutButton");
-    logout.onclick = onLogoutEventHandler;
-  }
-};
-
 function onDeleteEventHandler() {
-  console.log("HELLO");
   return confirm("Are you sure you want to delete this asset?");
 }
 
